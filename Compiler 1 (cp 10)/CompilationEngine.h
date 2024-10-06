1a18922d1cafa5738e6c10ca4e8d49a27b92c302
Pacifiky <87620151+infinitypacific@users.noreply.github.com>
2024-10-06 17:13:56 +0200

    Finished Tokenizer!
    Yay the tokenizer is finished!

---
+++ Compiler 1 (cp 10)/CompilationEngine.h
@@ -0,0 +1,19 @@
+﻿#ifndef PACIFIKY_JACKTOKENIZER_H
+#define PACIFIKY_JACKTOKENIZER_H
+#include <iostream>
+#include <string>
+#include <fstream>
+class CompilationEngine {
+	public:
+		CompilationEngine(std::ifstream &ifilein,std::ofstream &ofilein):ifile(ifilein),ofile(ofilein){
+			std::cout << "Init CEngine" << std::endl;
+		};
+		void CompileClass(){};
+		void CompileClassVarDec(){};
+		void CompileSubroutine(){};
+		void compileParameterList(){};
+	private:
+		std::ifstream &ifile;
+		std::ofstream &ofile;
+};
+#endif
---
+++ Compiler 1 (cp 10)/Compiler.cpp
@@ -0,0 +1,142 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdexcept>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <vector>
+//#include <memory>
+//#include <filesystem>
+using namespace std;
+#include "JackTokenizer.h"
+#include "CompilationEngine.h"
+
+string tokenTypeEnumDeb[] = {"KEYWORD","SYMBOL","IDENTIFIER","INT_CONST","STRING_CONST"};
+string keyWordEnumDeb[] = {"CLASS","METHOD","FUNCTION","CONSTRUCTOR","INT","BOOLEAN","CHAR","VOID","VAR","STATIC","FIELD","LET","DO","IF","ELSE","WHILE","RETURN","TRUE","FALSE","J_NULL","THIS"};
+
+class JackAnalyzer {
+	public:
+		JackAnalyzer(string file){
+			//Check if file is directory and iterate to create tokenizers
+			cout << file.length() << endl;
+			try{
+				if(file.length()>5 && file.substr(file.length()-5,5) == ".jack"){
+					//Initialize a stream for the file
+					/*
+					ifstream stfiles(file);
+					if(!(stfiles.is_open())){
+						throw invalid_argument("File not found");
+					}
+					*/
+					//files = new ifstream[1];
+					//filesize = 1;
+					//filecount = 1;
+					//files[0] = move(stfiles);
+					files.push_back(file);
+				} else if(file.find(".") == -1){
+					struct stat temp;
+					char* dir = &file[0];
+					if(stat(dir,&temp) != 0){
+						throw invalid_argument("Folder not found");
+					};
+					struct dirent *item;
+					//struct stat info;
+					DIR *folder = opendir(dir);
+					
+					if(folder==nullptr){
+						//cerr << "Folder open error!" << endl;
+						throw invalid_argument("Folder open error!");
+						//return -1;
+					}
+					
+					while((item=readdir(folder))){
+						string fname = string(item->d_name);
+						string fullfile = file+'/'+fname;
+						if(fname.length()>5 && fname.substr(fname.length()-5,5) == ".jack"){
+							files.push_back(fullfile);
+						};
+					};
+					
+				} else {
+					throw invalid_argument("Invalid format!");
+				};
+			}catch(const out_of_range& e){
+				cout << "Err with if!" << endl;
+			}
+			//Initialize stream for filewriting xml
+			cout << to_string(files.size()) << endl;
+			for(const string foldfil : files){
+				cout << "Init " << foldfil << endl;
+				ifstream foldfilin(foldfil);
+				if(isUTF(foldfilin)){cout<<"Processing format UTF-8"<<endl;}else{cout<<"Processing format ASCII"<<endl;};
+				ofstream foldfilout(foldfil.substr(0,foldfil.length()-5) + ".xml");
+				cout << foldfil.substr(0,foldfil.length()-5) + ".xml" << endl;
+				//JackTokenizer Tokeny(move(foldfilin));
+				JackTokenizer Tokeny(foldfilin);
+				/*
+				Tokeny.advance();
+				cout << to_string(static_cast<int>(Tokeny.tokenType)) << endl;
+				Tokeny.advance();
+				cout << to_string(static_cast<int>(Tokeny.tokenType)) << endl;
+				cout << Tokeny.identifier << endl;
+				Tokeny.advance();
+				cout << to_string(static_cast<int>(Tokeny.tokenType)) << endl;
+				*/
+				while(Tokeny.hasMoreTokens){
+					Tokeny.advance();
+					cout << tokenTypeEnumDeb[static_cast<int>(Tokeny.tokenType)] << endl;
+					switch(Tokeny.tokenType){
+						case tokenTypeEnum::KEYWORD:
+							cout << "Keyword: " << keyWordEnumDeb[static_cast<int>(Tokeny.keyWord)] << endl;
+							break;
+						case tokenTypeEnum::IDENTIFIER:
+							cout << "ID: " << Tokeny.identifier << endl;
+							break;
+						case tokenTypeEnum::SYMBOL:
+							cout << "Symbol: " << Tokeny.symbol << endl;
+							break;
+						case tokenTypeEnum::STRING_CONST:
+							cout << "SCon: " << Tokeny.stringVal << endl;
+							break;
+						case tokenTypeEnum::INT_CONST:
+							cout << "ICon: " << Tokeny.intVal << endl;
+							break;
+					}
+				};
+				//cout << to_string(static_cast<int>(tokenTypeEnum::IDENTIFIER)) << endl;
+			}
+			
+		}
+		/*
+		unsigned int filecount = 0;
+		unsigned int filesize = 0;
+		ifstream* files;
+		*/
+	private:
+		vector<string> files;
+		bool isUTF(ifstream &check){
+			char buff[3];
+			check.read(buff,3);
+			if(buff[0]==(char)0xEF && buff[1]==(char)0xBB && buff[2]==(char)0xBF){return true;}else{check.seekg(0);return false;};
+		}
+};
+
+int main(int argc, char* argv[]) {
+	if(argc<2){
+		cerr << "Insufficient arguments!";
+	} else if(argc>2){
+	  	cerr << "Too many arguments!";
+	} else {
+		try{
+			JackAnalyzer(string(argv[1]));
+		}catch(const out_of_range& e){
+			cout << "Err on Init" << endl;
+		}
+	};
+	/*
+	cout << 'JackCompiler>';
+	string filename << cin;
+	JackAnalyzer(filename);
+	*/
+	return 0;
+};
